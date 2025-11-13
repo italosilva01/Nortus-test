aedd35828c4861d935e98ac05cbef771da3f064e
italo silva <italo.silvan87@gmail.com>
2025-11-13 09:15:07 -0300

    feat: configure Jest and React Testing Library and add initial Page component test

---
+++ __test__/page.test.tsx
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page from '@/app/page'
+
+// Mock next-intl
+jest.mock('next-intl', () => ({
+  useTranslations: () => {
+    return (key: string) => {
+      const translations: Record<string, string> = {
+        'HomePage.title': 'Hello world!',
+      }
+      return translations[key] || key
+    }
+  },
+}))
+
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<Page />)
+ 
+    const heading = screen.getByRole('heading', { level: 1 })
+ 
+    expect(heading).toBeInTheDocument()
+  })
+})
+
