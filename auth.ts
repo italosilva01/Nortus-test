c1d28937c8dd69bdfe8721d7da210125ec85c7a9
italo silva <italo.silvan87@gmail.com>
2025-11-13 12:14:56 -0300

    feat: implement authentication with NextAuth and create middleware for route protection

---
+++ auth.ts
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+       if (credentials.username === "teste@teste.com" && 
+        credentials.password === "123456") {
+      return {
+        id: "1",
+        name: "Usuário Teste",
+        username: credentials.username,
+      }
+    }
+    return null
+      }
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+  },
+})
