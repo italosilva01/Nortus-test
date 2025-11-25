b710dc9bb843509ce88af92131a3ddeca9ba4dea
italo silva <italo.silvan87@gmail.com>
2025-11-25 12:57:48 -0300

    refactor: update layout and spacing in FormLogin component for improved responsiveness and visual consistency

--- feature/auth/login/ui/FormLogin.tsx
+++ feature/auth/login/ui/FormLogin.tsx
@@ -34,7 +34,7 @@ export const FormLogin = () => {
 
     return (
         <div className="flex flex-col items-start bg-app-bg-dark h-max">
-            <div className="w-full max-w-[8.125rem] xl:max-w-[9.375em] 2xl:max-w-[10.8825rem] h-auto">
+            <div className="w-full max-w-32.5 xl:max-w-[9.375em] 2xl:max-w-[10.8825rem] h-auto">
                 <Image
                     src={NortusLogo as string}
                     alt="Logo Nortus"
@@ -46,14 +46,14 @@ export const FormLogin = () => {
                     unoptimized
                 />
             </div>
-            <div className="mt-[7.5rem] 2xl:mt-[10rem] flex flex-col gap-[3.1875rem] 2xl:gap-[4.25rem] w-min">
+            <div className="mt-30 2xl:mt-40 flex flex-col gap-12.75 2xl:gap-17 w-min">
                 <div >
                     <Typography element="h1" fontColor="surface-solid" className="text-2xl xl:text-3xl 2xl:text-4xl line-height-[2.8631rem] letter-spacing-0px mb-3"
                     fontFamily="space-grotesk"
                     >{t('title')}</Typography>
                     <Typography element="p" fontFamily="inter" className="lg:text-base xl:text-lg 2xl:text-xl !line-height-[1.9375rem] letter-spacing-0.64px mt-1.5 " fontWeight="normal" fontColor="surface-solid">{t('description')}</Typography>
                 </div>
-                <form onSubmit={handleSubmit((data) => handleLogin(data))} className="w-full flex flex-col gap-7 md:max-w-[25rem] xl:max-w-[34.75rem] 2xl:max-w-[60.1875rem] xl:gap-[1.3125rem] 2xl:gap-7 mt-0.5">
+                <form onSubmit={handleSubmit((data) => handleLogin(data))} className="w-full flex flex-col gap-7 md:max-w-100 xl:max-w-139 2xl:max-w-240.75 xl:gap-5.25 2xl:gap-7 mt-0.5">
                     <FormInput
                         type="text"
                         placeholder="Usuário"
@@ -69,7 +69,7 @@ export const FormLogin = () => {
                     />
                     <div className="flex justify-between my-2">
                         <div className="flex items-center">
-                            <Checkbox id="terms" className="xl:size-[1.125rem] 2xl:size-[1.375rem] border-outline-solid font-medium border-[.0938rem]" />
+                            <Checkbox id="terms" className="xl:size-4.5 2xl:size-5.5 border-outline-solid font-medium border-[.0938rem]" />
                             <Label htmlFor="terms" className="text-white xl:text-[1rem] 2xl:!text-[1.125rem] !line-height-[1.5906rem] ml-4 !letter-spacing-0.13px font-space-grotesk">{t('remember')}</Label>
                         </div>
                         <Link href="/forgot-password" className="font-space-grotesk font-medium xl:text-[1rem] 2xl:!text-[1.125rem] !line-height-[1.5938rem] !letter-spacing-0.13px text-app-primary mr-5">{t('forgot')}</Link>
@@ -77,7 +77,7 @@ export const FormLogin = () => {
                     <Button
                         type="submit"
                         size="lg"
-                        className="w-full xl:h-[3.25rem 2xl:h-[4.25rem] rounded-[1.25rem] border-[.0794rem] !text-button-solid border-outline-solid xl:!text-[1rem] 2xl:text-[18px] xl:mt-[1.125rem] 2xl:mt-6"
+                        className="w-full xl:h-[3.25rem 2xl:h-17 rounded-[1.25rem] border-[.0794rem] !text-button-solid border-outline-solid xl:!text-[1rem] 2xl:text-[18px] xl:mt-[1.125rem] 2xl:mt-6"
                         variant="default"
                     >
                         {t('enter')}
