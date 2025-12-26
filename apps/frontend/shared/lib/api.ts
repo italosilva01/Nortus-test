0d8f5af3dfc30ce2d1275c277d2aa48845ddbe11
italo silva <italo.silvan87@gmail.com>
2025-12-26 19:22:01 -0300

    chore: update API baseURL to use mock API endpoint and remove commented localhost URL

--- apps/frontend/shared/lib/api.ts
+++ apps/frontend/shared/lib/api.ts
@@ -2,11 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://loomi.s3.us-east-1.amazonaws.com/mock-api-json/v2',
+  //baseURL: 'http://localhost:3001/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default api;
-
-    
