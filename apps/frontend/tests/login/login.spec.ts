fb7baf292bd801ad17bd51a207c88d323acc182e
italo silva <italo.silvan87@gmail.com>
2026-06-15 18:33:04 -0300

    fix: update Playwright configuration and enhance login tests
    - Changed baseURL in playwright.config.ts to a hardcoded localhost URL for local testing.
    - Refactored login.spec.ts to utilize baseURL for navigation and assertions, improving test reliability.
    - Added timeout to login assertion to handle potential delays in user authentication.

--- apps/frontend/tests/login/login.spec.ts
+++ apps/frontend/tests/login/login.spec.ts
@@ -2,19 +2,30 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Login', () => {
   test.describe('when the user is not logged in', () => {
-    test('Try access the dashboard', async ({ page }) => {
-      await page.goto('http://localhost:3000/dashboard');
+    test('Try access the dashboard', async ({ page, baseURL }) => {
+      await page.goto('/dashboard');
       await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
-      await expect(page.url()).toContain('/login');
+      await expect(page.url()).toBe(baseURL + '/login');
     });
   });
 
   test.describe('when the user is logged in', () => {
-    test.beforeAll(async ({ request }) => {
+    test('Try access the dashboard', async ({ page, baseURL }) => {
       // aqui irei fazer o login do usuário usando o setup de autenticação
-    });
-    test('Try access the dashboard', async ({ page }) => {
+      await page.goto('/login');
+      await page
+        .getByRole('textbox', { name: 'Digite seu usuário' })
+        .fill('Valeria.gonzalez+7476@eightroom.com');
+      await page
+        .getByRole('textbox', { name: 'Digite sua senha' })
+        .fill('User7476.');
 
+      await page.getByRole('button', { name: 'Entrar' }).click();
+      // Por algum motivo que não sei, o login não está sendoi realizado corretamente, por isso estou usando o timeout para esperar 10 segundos
+      await expect(page.url()).toBe(baseURL + '/', { timeout: 10_000 });
+      await expect(
+        page.getByRole('heading', { name: 'Dashboard' }),
+      ).toBeVisible();
     });
   });
 });
